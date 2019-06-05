388874aef9e20ca549c19b89b99162aac253b137
Rafael <Rafael@192.168.70.20>
2019-06-05 19:29:05 -0300

    Teste

--- src/main/java/com/br/springthymeleaf/webapp/WebappApplication.java
+++ src/main/java/com/br/springthymeleaf/webapp/WebappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "com.br.springthymeleaf.webapp.controller , com.br.springthymeleaf.webapp.service", "controller", "service" } )
+@ComponentScan
 // If our Controller class or Service class is not in the same packages we have //to add packages's name like this...directory(package) with main class
 public class WebappApplication {
 	public static void main(String[] args) {
