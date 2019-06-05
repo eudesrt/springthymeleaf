8dadfb9d8f2cbbf7092d82cc24b8105969b88f06
Rafael <Rafael@192.168.70.20>
2019-06-05 19:48:18 -0300

    testes

--- src/main/java/com/br/springthymeleaf/webapp/WebappApplication.java
+++ src/main/java/com/br/springthymeleaf/webapp/WebappApplication.java
@@ -2,10 +2,8 @@ package com.br.springthymeleaf.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"model", "service"})
 public class WebappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
